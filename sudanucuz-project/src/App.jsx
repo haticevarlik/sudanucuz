e6c0cc59d0cb6415802062140a207d22c3ca5637
haticevarlik <haticevarlik343@gmail.com>
2025-06-11 12:26:42 +0300

    vercel

--- sudanucuz-project/src/App.jsx
+++ sudanucuz-project/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <div className="bg-blue-500 text-white p-4 rounded-lg">
+        Hello, Tailwind!
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
